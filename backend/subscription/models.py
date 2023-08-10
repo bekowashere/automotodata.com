34be501626fb211715ade02b2d4b7f0d4778e564
Berke <uberke.karatas@gmail.com>
2023-08-10 18:54:31 +0300

    subscription/ subscription class

--- backend/subscription/models.py
+++ backend/subscription/models.py
@@ -37,3 +37,58 @@ class PlanFeature(models.Model):
     class Meta:
         verbose_name = _('Plan Feature')
         verbose_name_plural = _('Plan Features')
+
+class Subscription(models.Model):
+    FREE = 'F'
+    MONTHLY = 'M'
+    YEARLY = 'Y'
+
+    PERIOD_CHOICES = [
+        (FREE, 'Free Trial'),
+        (MONTHLY, 'Monthly Subscription'),
+        (YEARLY, 'Yearly Subscription')
+    ]
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_subscription",
+        verbose_name=_('User')
+    )
+
+    plan = models.ForeignKey(
+        Plan,
+        on_delete=models.CASCADE,
+        related_name="plan_subscriptions",
+        verbose_name=_('Plan')
+    )
+
+    period_type = models.CharField(
+        _('Period Type'),
+        max_length=1,
+        choices=PERIOD_CHOICES,
+        default=FREE
+    )
+    period_duration = models.PositiveSmallIntegerField(
+        _('Period Duration'),
+        help_text=_('The default value is 30 because a month has 30 days. expiry_date = start_date + period_duration'),
+        default=30
+    )
+
+    start_date = models.DateField(_('Start Date'), default=date.today())
+    expiry_date = models.DateField(_('Expiry Date'), null=True, blank=True)
+
+    paid_amount = models.DecimalField(
+        _('Paid Amount'),
+        max_digits=9,
+        decimal_places=2
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return f"{self.user.email} - {self.plan.name}"
+    
+    class Meta:
+        verbose_name = _('Subscription')
+        verbose_name_plural = _('Subscriptions')
